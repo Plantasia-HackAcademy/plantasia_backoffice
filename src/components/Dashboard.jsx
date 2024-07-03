7c4643c15154fe26352f68b852c4a5c4a17d0ac6
Iván De León Lino <ivandeleon_2301@hotmail.com>
2024-07-03 15:53:40 -0300

    Fixed routes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,18 +2,25 @@ import { DashboardTable } from "./DashboardTable";
 import { SalesCard } from "./SalesCard";
 import "../css/Dashboard.css";
 import { Outlet } from "react-router";
+import { useLocation } from "react-router";
 
 export const Dashboard = ({ title }) => {
+  const location = useLocation();
+
   return (
     <main className="main-container">
       <h2 className="dashboard__title">{title}</h2>
-      <h3 className="dashboard__past-days">Last 30 days</h3>
 
-      <div className="cards-container">
-        <SalesCard />
-        <SalesCard />
-        <SalesCard />
-      </div>
+      {location.pathname === "/dashboard" && (
+        <>
+          <h3 className="dashboard__past-days">Last 30 days</h3>
+          <div className="cards-container">
+            <SalesCard />
+            <SalesCard />
+            <SalesCard />
+          </div>
+        </>
+      )}
       <DashboardTable />
       <Outlet />
     </main>
